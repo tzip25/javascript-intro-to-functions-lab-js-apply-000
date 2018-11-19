dfdff6cfcd43680f26daa0f3aaa6788a25755173
Tali Scheer <tali.scheer@gmail.com>
2018-11-19 22:28:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var lowercase = "hello";
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === lowercase) {
     return `I can't hear you!`;
-  } if (string.toUpperCase() === uppercase) {
+  } else if (string.toUpperCase() === uppercase) {
     return `YES INDEED!`;
     } else if (string === "I love you, Grandma.") {
     return `I love you, too.`;
