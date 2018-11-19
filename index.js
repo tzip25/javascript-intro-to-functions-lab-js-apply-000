9e4e3faeeb4536774f46a679d2b720314f38666f
Tali Scheer <tali.scheer@gmail.com>
2018-11-19 22:29:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,3 +28,17 @@ function sayHiToGrandma(string) {
     return `I love you, too.`;
   }
 }
+
+
+var uppercase = "HELLO";
+var lowercase = "hello";
+  
+  function sayHiToGrandma(string) {
+    if (string === lowercase) {
+      return `I can't hear you!`;
+      } else if (string === uppercase) {
+      return `YES INDEED!`;
+      } else if (string === "I love you, Grandma.") {
+        return `I love you, too.`;
+    }
+  }
